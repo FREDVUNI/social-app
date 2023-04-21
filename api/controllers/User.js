42d7c454c841ececf5d6e8567d078d1632f5e0b7
FREDVUNI <fredvuni809@gmail.com>
2023-04-21 16:45:21 +0300

    update user profile

--- api/controllers/User.js
+++ api/controllers/User.js
@@ -20,3 +20,34 @@ export const getUser = async (req, res) => {
     res.status(500).json(error.message);
   }
 };
+
+export const updateProfile = async (req, res) => {
+  try {
+    const token = req.cookies.accessToken;
+    if (!token) return res.status(401).json("You're not authorized.");
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, userInfo) => {
+      if (err) return res.status(403).json("You're not authorized.");
+
+      const q =
+        "UPDATE USER SET `name` = ?,`username` = ?,`city` = ?,`coverImage` = ?,`profileImage` = ? WHERE `id` = ?";
+
+      const values = [
+        req.body.name,
+        req.body.username,
+        req.body.city,
+        req.body.coverImage,
+        req.body.profileImage,
+        userInfo.id,
+      ];
+
+      db.query(q, [values], (err, data) => {
+        if (err) return res.status(500).json(err);
+        if (data.affectedRows > 0)
+          return res.status(200) / json("profile has been updated.");
+      });
+    });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
